626247302b381d58f84eecac0fe29a79a047a3fc
Liming <emailforlm@163.com>
2017-11-03 11:24:10 +0800

    log4j test

--- src/main/java/com/lm/ssm/dao/impl/UserDaoImpl.java
+++ src/main/java/com/lm/ssm/dao/impl/UserDaoImpl.java
@@ -3,6 +3,7 @@ package com.lm.ssm.dao.impl;
 import com.lm.ssm.dao.UserDao;
 import com.lm.ssm.entity.User;
 import com.lm.ssm.mapper.UserMapper;
+import org.apache.log4j.Logger;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -15,11 +16,13 @@ import javax.annotation.Resource;
 @Repository
 public class UserDaoImpl implements UserDao {
 
+    private static Logger logger = Logger.getLogger(UserDao.class);
     @Resource
     private SqlSessionTemplate sqlSessionTemplate;
 
 
     public void save(User user) {
+        logger.info("save the user "+ user.getUsername());
         UserMapper mapper =sqlSessionTemplate.getMapper(UserMapper.class);
         mapper.save(user);
     }
